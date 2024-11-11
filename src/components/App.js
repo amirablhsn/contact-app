b5e298dc1bc25c3e4a30987d9b3cc66365ca52e8
Amir Abulhassan <amirabulhassn@gmail.com>
2024-11-11 00:38:23 -0500

    Deleted unwanted files and added components folder with necessary files

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+
+  const contacts = [
+    {
+      id: "1",
+      name: "Amir",
+      email: "email@example.com"
+    },
+  ];
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts}/>
+    </div>
+  );
+};
+
+export default App;
